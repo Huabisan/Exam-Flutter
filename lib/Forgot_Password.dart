d2a76e686f359f32db25a584a58c2376a8ce48a9
Reika <75054515+arvanda@users.noreply.github.com>
2024-02-26 15:44:13 +0700

    Add files via upload

---
+++ lib/Forgot_Password.dart
@@ -0,0 +1,111 @@
+import 'package:exam_flutter2/login.dart';
+import 'package:flutter/material.dart';
+
+void login() {
+  runApp(Forgot());
+}
+
+class Forgot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: LoginForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _isPasswordVisible = false;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordVisible = !_isPasswordVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 40.0),
+        Text(
+          'Forgot',
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10), // Add spacing between the texts
+        Text(
+          'Password ?',
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 30.0),
+        TextFormField(
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            hintText: 'Email or Username',
+            labelText: 'Email or Username',
+            border: OutlineInputBorder(),
+            prefixIcon: Image.asset('img/Mail.png'),
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Align(
+          alignment: AlignmentDirectional.centerStart,
+          child: Row(
+            children: [
+              Text('* ',
+              style: TextStyle(
+              color: Colors.orange,
+              ),
+              ),
+              Text('We will send you a message to set or reset')
+            ],
+          ),
+        ),
+        Column(
+          children: [
+            Text('your new password')
+          ],
+        ),
+        SizedBox(height: 10.0),
+        Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xffF83758),
+              minimumSize: Size(317.0, 55.0),
+            ),
+            onPressed: () {
+              print('Login button tapped');
+            },
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'Register.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      // Navigate to the get started screen after 4 seconds
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => GetStartedScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'img/logoipsum-255 1 (1).png', // Your logo image path
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GetStartedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('img/unsplash_fouVDmGXoPI.png'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)
+          ),
+        ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 40.0), // Adjust as per your desired spacing
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'You want Authentic, here you go!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'Find it here, buy it now!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10.0),// Add some spacing between the texts and the button
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffF83758),
+                    minimumSize: Size(317.0, 55.0),
+                  ),
+                  onPressed: () {
+                    // Navigate to the login page when the button is pressed
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => login2()),
+                    );
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
